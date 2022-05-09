87cdd1e606e40cb0812de7ddd6c1e6cce70b12f0
terrytrl <73771668+terrytrl@users.noreply.github.com>
2022-05-10 02:22:11 +1000

    update

--- ReadFile.java
+++ ReadFile.java
@@ -2,78 +2,79 @@ import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.HashMap;
 
 public class ReadFile {
 
-    private String string;
-    private String string2;
     private SimpleSentence s1;
     private SimpleSentence s2;
     private SimpleSentence s3;
-    private ArrayList<Sentence> sentence;
-    private ArrayList<Sentence> sentence2;
+    private ComplexSentence cs1;
+    private ComplexSentence cs2;
+    private String con1;
+    private String con2;
+    private String[] string2;
 
-    private ComplexSentence testComplexSentence;
-    private ComplexSentence testComplexSentence2;
+    String[] temp;
+
+    private String[] splitString;
+    private ArrayList<Sentence> list;
+    private ArrayList<Sentence> listComplex;
+
+    private Map<String, SimpleSentence> hashMap = new HashMap<String, SimpleSentence>();
 
     // creates empty knowledge base
     public ReadFile() {
+
     }
 
     public void readInput(BufferedReader r) {
+
         try {
             r.readLine();
-
             String tellString = r.readLine();
             tellString = tellString.replaceAll("\\s", "");
             String[] strings = tellString.split(";");
-            String[] string2;
-
+            
+            
             // for (int i = 0; i < strings.length; i++) {
+            //     string2 = strings[i].split("=>|&");
+            //     for (String s : string2) {
+            //         s1 = new SimpleSentence(s);
+            //     System.out.println(hashMap);
 
-            // if (strings[i].contains("=>") || strings[i].contains("&")) {
-
-            // for (String s : strings) {
-            // if (s.matches("=>")) {
-            // string = "=>";
-            // }
-            // }
-            // for (String s : strings) {
-            // if (s.matches("&")) {
-            // string = "&";
-            // }
+            //         hashMap.put(s, s1);
+            //     }
             // }
 
-            // string2 = strings[i].split("=>|&");
-            // for (String s : string2) {
-            // // System.out.println(s);
-            // s1 = new SimpleSentence(s);
-            // System.out.println(s1.toString());
-            // }
-
-            // } else {
-            // System.out.println(strings[i]);
-            // }
-
-            // }
-            for (int i = 0; i < strings.length; i++) {
-                    if (strings[i].find("\\[.]")) {
-                        s1 = new SimpleSentence(strings[i]);
-                        System.out.println(s1.toString());
+            for(int i = 0; i < strings.length; i++){
+                
+                
+                for(String s : temp){
+                    if( s.contains("=>")){
+                        s3 = new SimpleSentence(temp[0]);
+                        list.add(s3);
+                        con2 = "=>";
+                        cs2 = new ComplexSentence(list, con2);
+                        if(strings[i].contains("&")){
+                            list.clear();
+                            temp = strings[i].split("&");
+                            s1 = new SimpleSentence(temp[0]);
+                            list.add(s1);
+                            s2 = new SimpleSentence(temp[1]);
+                            con1 = "=>";
+                            list.add(s2);
+                            
+                            cs1 = new ComplexSentence(list, con1);
+                        }
+                        
                     }
-                    // if (strings[i].contains("[a-zA-Z0-9]")) {
-                    // s3 = new SimpleSentence(strings[i]);
-                    // sentence2.add(s3);
-                    // }
-                // if (strings[i].contains("=>")) {
-                // string2 = strings[i];
-                // testComplexSentence2 = new ComplexSentence(sentence2, string2);
-                // }
-                // System.out.println(testComplexSentence.toString());
+                }
 
-                sentence.removeAll(sentence);
-                sentence2.removeAll(sentence2);
+
+                System.out.println(hashMap);
             }
+
         } catch (IOException ioe) {
             ioe.printStackTrace();
             System.exit(2);
@@ -81,8 +82,8 @@ public class ReadFile {
     }
 
     public ReadFile(BufferedReader r) {
-        sentence = new ArrayList<Sentence>();
-        sentence2 = new ArrayList<Sentence>();
+        // sentence = new ArrayList<Sentence>();
+        // sentence2 = new ArrayList<Sentence>();
         readInput(r);
     }
 }
