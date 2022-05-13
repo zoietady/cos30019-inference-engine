d7cf47b2fba464dcb8bee29c43703f7afe48e859
terrytrl <73771668+terrytrl@users.noreply.github.com>
2022-05-13 18:22:39 +1000

    update

--- ReadFile.java
+++ ReadFile.java
@@ -2,15 +2,23 @@ import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.naming.LimitExceededException;
+
 import java.util.HashMap;
 
 public class ReadFile {
 
-    private SimpleSentence s1;
-    private SimpleSentence s2;
-    private SimpleSentence s3;
+    private SimpleSentence ss1;
+    private SimpleSentence ss2;
+    private SimpleSentence ss3;
+    private SimpleSentence ss4;
+    private SimpleSentence ss_lone;
     private ComplexSentence cs1;
     private ComplexSentence cs2;
+
+    private String[] s2;
+
     private String con1;
     private String con2;
     private String[] string2;
@@ -18,7 +26,8 @@ public class ReadFile {
     String[] temp;
 
     private String[] splitString;
-    private ArrayList<Sentence> list;
+    private ArrayList<Sentence> listOfSS1;
+    private ArrayList<Sentence> listOfSS2;
     private ArrayList<Sentence> listComplex;
 
     private Map<String, SimpleSentence> hashMap = new HashMap<String, SimpleSentence>();
@@ -26,55 +35,42 @@ public class ReadFile {
     // creates empty knowledge base
     public ReadFile() {
 
+        listComplex = new ArrayList<Sentence>();
     }
 
     public void readInput(BufferedReader r) {
-
         try {
             r.readLine();
             String tellString = r.readLine();
             tellString = tellString.replaceAll("\\s", "");
             String[] strings = tellString.split(";");
-            
-            
-            // for (int i = 0; i < strings.length; i++) {
-            //     string2 = strings[i].split("=>|&");
-            //     for (String s : string2) {
-            //         s1 = new SimpleSentence(s);
-            //     System.out.println(hashMap);
-
-            //         hashMap.put(s, s1);
-            //     }
-            // }
-
-            for(int i = 0; i < strings.length; i++){
-                
-                
-                for(String s : temp){
-                    if( s.contains("=>")){
-                        s3 = new SimpleSentence(temp[0]);
-                        list.add(s3);
-                        con2 = "=>";
-                        cs2 = new ComplexSentence(list, con2);
-                        if(strings[i].contains("&")){
-                            list.clear();
-                            temp = strings[i].split("&");
-                            s1 = new SimpleSentence(temp[0]);
-                            list.add(s1);
-                            s2 = new SimpleSentence(temp[1]);
-                            con1 = "=>";
-                            list.add(s2);
-                            
-                            cs1 = new ComplexSentence(list, con1);
-                        }
-                        
-                    }
+            listComplex = new ArrayList<>();
+            for (String s : strings) {
+                if (s.contains("=>")) {
+                    listOfSS1 = new ArrayList<Sentence>();
+                    s2 = s.split("=>");
+                    ss3 = new SimpleSentence(s2[1]);
+                    con1 = "=>";
+                    listOfSS1.add(ss3);
+                    cs1 = new ComplexSentence(listOfSS1, con1);
                 }
+                if (s2[0].contains("&")) {
+                    listOfSS2 = new ArrayList<Sentence>();
+                    s2 = s.split("&");
+                    con2 = "&";
+                    ss2 = new SimpleSentence(s2[0]);
+                    ss3 = new SimpleSentence(s2[1]);
+                    listOfSS2.add(ss2);
+                    listOfSS2.add(ss3);
+                    cs1 = new ComplexSentence(listOfSS2, con2);
+                }
+                listComplex.add(cs1);
+                listComplex.add(cs2);
 
+                System.out.println(listComplex);
 
-                System.out.println(hashMap);
             }
-
+            System.out.println(cs1.toString());
         } catch (IOException ioe) {
             ioe.printStackTrace();
             System.exit(2);
